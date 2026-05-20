9da35612ade683c2bf87986204df6881a970d313
manonganvisit-06 <manonganvisit@gmail.com>
2026-05-21 00:38:17 +0700

    update favicon and metadata

--- src/app/kalkulator/page.tsx
+++ src/app/kalkulator/page.tsx
@@ -1,6 +1,12 @@
 import Navbar from "@/components/Navbar";
 import Kalkulator from "@/components/Kalkulator";
 import Footer from "@/components/Footer";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Kalkulator",
+  description: "Kalkulator hitung harga sampah per kilo",
+};
 
 export default function Calculator() {
   return (
